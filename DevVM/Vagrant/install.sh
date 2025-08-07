dbfad0979e070a98cd9fa0c48534e225ab05c01c
Alex Kling <m2kling@gmail.com>
2025-08-07 18:33:18 +0200

    Added setup through VMbox

---
+++ DevVM/Vagrant/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copy sshd_config file into the VM
+# scp -P 4243 -r /home/mkling/Documents/Inception/DevVM/Vagrant/sshd_config mkling@localhost:/home/mkling/.
+
+# SSH connect into the VM
+# ssh -X mkling@localhost -p 4243
+
+# Add Sudo and add User to it / Need privilege/root
+su
+apt install sudo
+usermod -a -G sudo mkling
+
+# Set up SSH
+sudo mv sshd_config /etc/ssh/sshd_config
+sudo ssh-keygen -A
+
+# Install VS-Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+
+# Install Firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt-get update
+sudo apt-get install firefox
+
+# Install Docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
