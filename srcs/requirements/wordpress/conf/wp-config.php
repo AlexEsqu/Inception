b8efa99a013d1830abb6b463c58fa9802b2877b5
Alex <momiji.kling@gmail.com>
2025-08-22 09:03:22 +0200

    persisting is hard

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -2,16 +2,15 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
  *
  * This file contains the following configurations:
  *
- * * Database settings
+ * * MySQL settings
  * * Secret keys
  * * Database table prefix
- * * Localized language
  * * ABSPATH
  *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
@@ -19,61 +18,60 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_NAME', 'db1' );
 
-/** Database username */
-define( 'DB_USER', 'wordpress_user' );
+/** MySQL database username */
+define( 'DB_USER', 'user' );
 
-/** Database password */
-define( 'DB_PASSWORD', 'placeholder' );
+/** MySQL database password */
+define( 'DB_PASSWORD', 'pwd' );
 
-/** Database hostname */
+/** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
 
-/** Database charset to use in creating database tables. */
+/** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
-/** The database collate type. Don't change this if in doubt. */
+/** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'WP_ALLOW_REPAIR', true );
+
 /**#@+
- * Authentication unique keys and salts.
+ * Authentication Unique Keys and Salts.
  *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',          'H1vGL@`5W~3&*#{@(Rd5.c<P];&V,m4eRrxGC,559A/9jX^#klARlZuh5i(s^9N1' );
-define( 'SECURE_AUTH_KEY',   '^,e$@p]re%vxT?{pSe;5} ]R+EM7T4Z,I/M{-!(v~s/yNrZ}[^}hfFzL-PArj1NO' );
-define( 'LOGGED_IN_KEY',     '{=q$3m&m_)w1GO/NRWmp[!i9|e/nqG<.}V.Q2XylUnw2nt&pIVtD>2Or]sud}er ' );
-define( 'NONCE_KEY',         'YH&(L 7C^<se[*f;E26I|b:h#kd0@0K i:-6GEEjW!h Uy:|`}`p*0/yDEK+`{5m' );
-define( 'AUTH_SALT',         '-Mv@U7j-,G{VF{2}H{V$}5.jMiYMNFRm4@Uu[MdoiHp,2zZE8HvyiDL-WCvuD{*L' );
-define( 'SECURE_AUTH_SALT',  '#)#+4p30]UvjTdlGw]!7W%Vg^fD9ZM[l%nt!AUWwN0`H/.0_#3oj0`WMb|[3_)9)' );
-define( 'LOGGED_IN_SALT',    '$`ZpfuyT<3G|K;ZR&0 @%q/+DoP,^]QL:Bt|qhF>aKgeQ}(V32|yNsr!aS%|1;tS' );
-define( 'NONCE_SALT',        '3Y5keWGSk;>S9~ @pThss7!9KaN.ClI/+7S1!Ky*n3~P;uOrhoK@OqL=3eK>Y@a0' );
-define( 'WP_CACHE_KEY_SALT', 'h%0l*G%1w2f!IUf5DmD&&qIM%nq8?Q/n)HIq*QdK xg5pM$faF8-sYYU<(Xgjzt.' );
+define( 'AUTH_KEY',         '):Uw9 :|7$m3yy=c^IM%d8}zG6yXY%25SDUyr.r#GcDP)[b25Yn$sDLNwR~I=kwq' );
+define( 'SECURE_AUTH_KEY',  'lBWxAzhu=StQ(s-[t_D8yH8_`0NiM~d[m q<{Hri]n#UM3J;@x[ne;,k<~cN`~%,' );
+define( 'LOGGED_IN_KEY',    ' /e+%ecWs`>hA<s`|+7rmujt>3MA}GD*n=D7W%$8h*Xc!jP?hn+fw0#;;g{Ywl@k' );
+define( 'NONCE_KEY',        ' -cX{xQc|GjD$=kXd,|lUX5)*oT)ru3^px-iU{q;`1If22EqIwA0/lPIIbpbtB=C' );
+define( 'AUTH_SALT',        'U9LX s1@q6$[*VV,MUhL7tS@;I9t_u*uDQIfZdG.ei1Amy$*.RI_TSTz#y=X.>Wq' );
+define( 'SECURE_AUTH_SALT', '0<MR&l4v=cZ)8Ke/#ip>2<Ed@ j<#pvLaOMc-jEFM9^tr`X*T2qDIB@)gg.0<e2V' );
+define( 'LOGGED_IN_SALT',   'xSHh4B]r[~)h%n$f(dCt;mD}#q gy$<{ >qGgPS>XH*]jH>W<!10>H<_16l{(OdP' );
+define( 'NONCE_SALT',       '7Ea$kvU|lkO8&X]b7^#K+w! lH2)SOelLiaYYX(Zz)Ebk_]-#m,J&aM<*JedFa| ' );
 
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', 6379 );
+
+
+define('WP_CACHE', true);
 
 /**#@-*/
 
 /**
- * WordPress database table prefix.
+ * WordPress Database Table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
-
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-
 
 /**
  * For developers: WordPress debugging mode.
@@ -87,22 +85,15 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-if ( ! defined( 'WP_DEBUG' ) ) {
-        define( 'WP_DEBUG', false );
-}
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-        define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
-define('DISALLOW_FILE_EDIT', true);
-
-/** Redis specific settings */
-define('WP_REDIS_HOST', 'redis');
-define('WP_REDIS_PORT', '6379');
-define('WP_REDIS_DATABASE', '0');
+?>
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,43 +1,11 @@
 #!/bin/sh
+set -x
 
 DB_ROOT_PASSWORD=$(cat /run/secrets/mariadb_root_password)
 DB_PASSWORD=$(cat /run/secrets/mariadb_user_password)
 WP_ROOT_PASSWORD=$(cat /run/secrets/wordpress_root_password)
 WP_PASSWORD=$(cat /run/secrets/wordpress_user_password)
 echo "Secrets loaded successfully : ${MARIADB_DATABASE_NAME} ${MARIADB_USER} ${DB_ROOT_PASSWORD} ${DB_PASSWORD}"
-
-echo "Waiting for MariaDB to be ready..."
-MAX_TRIES=30
-TRIES=0
-
-while [ $TRIES -lt $MAX_TRIES ]; do
-	if mysql -h"$MARIADB_DATABASE_NAME" -u"$MARIADB_USER" -p"$DB_PASSWORD" 2>/dev/null; then
-		echo "Basic database connection successful!"
-
-		if mysql -h"$MARIADB_DATABASE_NAME" -u"$MARIADB_USER" -p"$DB_PASSWORD" "$WORDPRESS_DB_NAME" -e "SELECT 1;" 2>/dev/null; then
-			echo "Database access confirmed!"
-			break
-		else
-			echo "Can connect to MySQL but cannot access database '$WORDPRESS_DB_NAME'"
-			echo "Available databases:"
-			mysql -h"$MARIADB_DATABASE_NAME" -u"$MARIADB_USER" -p"$DB_PASSWORD" -e "SHOW DATABASES;" 2>/dev/null || echo "Cannot list databases"
-		fi
-	fi
-	sleep 2
-	TRIES=$((TRIES + 1))
-done
-
-if [ $TRIES -eq $MAX_TRIES ]; then
-	echo "ERROR: Could not connect to MariaDB after $MAX_TRIES attempts"
-	echo "Final debugging information:"
-	echo "Network test:"
-	nc -zv "$MARIADB_DATABASE_NAME" 3306
-	echo "Connection attempt output:"
-	mysql -h"$MARIADB_DATABASE_NAME" -u"$MARIADB_USER" -p"$DB_PASSWORD" "$WORDPRESS_DB_NAME" -e "SELECT 1;" 2>&1
-	exit 1
-fi
-
-echo "Database connection and access successful. Setting up WordPress..."
 
 cd /var/www/html
 
@@ -62,6 +30,12 @@ if [ ! -f wp-config.php ]; then
 		--admin_email="$WORDPRESS_ADMIN_EMAIL" \
 		--allow-root
 
+	echo "Creating a wordpress user..."
+	wp user create $WORDPRESS_USER $WP_EMAIL \
+		--role=author \
+		--user_pass=$WP_PASSWORD \
+		--allow-root
+
 	echo "Modifying the www.conf to allow connections from nginx..."
 	sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g'  /etc/php83/php-fpm.d/www.conf
 
@@ -75,53 +49,53 @@ if [ ! -f wp-config.php ]; then
 	wp option update blogname 'Inception'
 	wp option update blogdescription 'Portfolio : Programming & Graphics Projects'
 
-	HTML_DIR="/var/www/html/pages"
-	wp post delete $(wp post list --post_type=page --format=ids --allow-root) --force --allow-root
+	# HTML_DIR="/var/www/html/pages"
+	# wp post delete $(wp post list --post_type=page --format=ids --allow-root) --force --allow-root
 
-	if [ -f /var/www/html/pages/index.html ]; then
-	    echo "Creating Home page from index.html..."
-	    content=$(cat /var/www/html/pages/index.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
-	    wp post create \
-	        --post_type=page \
-	        --post_title="Home" \
-	        --post_status=publish \
-	        --post_content="$content" \
-	        --allow-root
+	# if [ -f /var/www/html/pages/index.html ]; then
+	#     echo "Creating Home page from index.html..."
+	#     content=$(cat /var/www/html/pages/index.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
+	#     wp post create \
+	#         --post_type=page \
+	#         --post_title="Home" \
+	#         --post_status=publish \
+	#         --post_content="$content" \
+	#         --allow-root
 
-	    HOME_PAGE_ID=$(wp post list --post_type=page --post_title="Home" --format=ids --allow-root)
-	    wp option update show_on_front 'page' --allow-root
-	    wp option update page_on_front "$HOME_PAGE_ID" --allow-root
+	#     HOME_PAGE_ID=$(wp post list --post_type=page --post_title="Home" --format=ids --allow-root)
+	#     wp option update show_on_front 'page' --allow-root
+	#     wp option update page_on_front "$HOME_PAGE_ID" --allow-root
 
-	    echo "Set Home page as front page (ID: $HOME_PAGE_ID)"
-	fi
+	#     echo "Set Home page as front page (ID: $HOME_PAGE_ID)"
+	# fi
 
-	if [ -f /var/www/html/pages/about-me.html ]; then
-	    echo "Creating About Me page from about-me.html..."
-	    content=$(cat /var/www/html/pages/about-me.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
-	    wp post create \
-	        --post_type=page \
-	        --post_title="About Me" \
-	        --post_status=publish \
-	        --post_content="$content" \
-	        --allow-root
-	fi
+	# if [ -f /var/www/html/pages/about-me.html ]; then
+	#     echo "Creating About Me page from about-me.html..."
+	#     content=$(cat /var/www/html/pages/about-me.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
+	#     wp post create \
+	#         --post_type=page \
+	#         --post_title="About Me" \
+	#         --post_status=publish \
+	#         --post_content="$content" \
+	#         --allow-root
+	# fi
 
-	if [ -f /var/www/html/pages/portfolio.html ]; then
-	    echo "Creating Portfolio page from portfolio.html..."
-	    content=$(cat /var/www/html/pages/portfolio.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
-	    wp post create \
-	        --post_type=page \
-	        --post_title="Portfolio" \
-	        --post_status=publish \
-	        --post_content="$content" \
-	        --allow-root
-	fi
+	# if [ -f /var/www/html/pages/portfolio.html ]; then
+	#     echo "Creating Portfolio page from portfolio.html..."
+	#     content=$(cat /var/www/html/pages/portfolio.html | sed 's/"/\\"/g' | sed 's/\\/\\\\/g')
+	#     wp post create \
+	#         --post_type=page \
+	#         --post_title="Portfolio" \
+	#         --post_status=publish \
+	#         --post_content="$content" \
+	#         --allow-root
+	# fi
 
 	wp plugin install redis-cache --activate --allow-root
 	wp redis enable --allow-root
 
-	# wp rewrite structure '/%postname%/' --hard --allow-root
-	# wp rewrite flush --allow-root
+	wp rewrite structure '/%postname%/' --hard --allow-root
+	wp rewrite flush --allow-root
 
 	echo "WordPress portfolio blog setup complete!"
 	echo "WordPress installation completed!"
