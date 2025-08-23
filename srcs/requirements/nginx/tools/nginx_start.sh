d7cc645b457fe0fbb9b0ec3c43060e50606655c0
Alex <momiji.kling@gmail.com>
2025-08-23 19:49:58 +0200

    Grafana finally connects yessss

--- srcs/requirements/nginx/tools/nginx_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-	-keyout /etc/nginx/ssl/nginx.key \
-	-out /etc/nginx/ssl/nginx.crt \
-	-subj "/C=FR/L=Paris/O=42/CN={WORDPRESS_DOMAIN}"
-
